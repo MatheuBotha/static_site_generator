f1a36668e16d83126fe55f17b0cda2fa04a118b7
MatheuBotha <matheu.botha@gmail.com>
2024-03-21 19:31:00 +0200

    ADD: link and image text node extraction

--- src/test_converter.py
+++ src/test_converter.py
@@ -9,38 +9,38 @@ from textnode import (
     text_type_image,
     text_type_link,
 )
-from converter import text_node_to_html_node, split_nodes_delimiter
+import converter
 
 
 class TestConverter(unittest.TestCase):
     def test_text_node_to_html_node_text_type(self):
         text_node = TextNode('myval', 'text', None)
-        html_node = text_node_to_html_node(text_node)
+        html_node = converter.text_node_to_html_node(text_node)
         expected = 'myval'
         self.assertEqual(html_node.to_html(), expected)
 
     def test_text_node_to_html_node_format_type(self):
         text_node = TextNode('myval', 'bold', None)
-        html_node = text_node_to_html_node(text_node)
+        html_node = converter.text_node_to_html_node(text_node)
         expected = '<b>myval</b>'
         self.assertEqual(html_node.to_html(), expected)
 
     def test_text_node_to_html_node_link_type(self):
         text_node = TextNode('myval', 'link', 'http://my_url')
-        html_node = text_node_to_html_node(text_node)
+        html_node = converter.text_node_to_html_node(text_node)
         expected = '<a href="http://my_url">myval</a>'
         self.assertEqual(html_node.to_html(), expected)
 
     def test_text_node_to_html_node_image_type(self):
         text_node = TextNode('myval', 'image', 'http://my_url')
-        html_node = text_node_to_html_node(text_node)
+        html_node = converter.text_node_to_html_node(text_node)
         expected = '<img src="http://my_url" alt="myval"></img>'
         self.assertEqual(html_node.to_html(), expected)
 
 
     def test_split_nodes_delimiter(self):
         node = TextNode("This is text with a `code block` word", text_type_text)
-        new_nodes = split_nodes_delimiter([node], "`", text_type_code)
+        new_nodes = converter.split_nodes_delimiter([node], "`", text_type_code)
         expected = [
             TextNode("This is text with a ", text_type_text),
             TextNode("code block", text_type_code),
@@ -50,7 +50,7 @@ class TestConverter(unittest.TestCase):
 
     def test_split_nodes_delimiter_at_start(self):
         node = TextNode("`code block` This is text with a", text_type_text)
-        new_nodes = split_nodes_delimiter([node], "`", text_type_code)
+        new_nodes = converter.split_nodes_delimiter([node], "`", text_type_code)
         expected = [
             TextNode("code block", text_type_code),
             TextNode(" This is text with a", text_type_text),
@@ -59,7 +59,7 @@ class TestConverter(unittest.TestCase):
 
     def test_split_nodes_delimiter_at_end(self):
         node = TextNode("This is text with a `code block`", text_type_text)
-        new_nodes = split_nodes_delimiter([node], "`", text_type_code)
+        new_nodes = converter.split_nodes_delimiter([node], "`", text_type_code)
         expected = [
             TextNode("This is text with a ", text_type_text),
             TextNode("code block", text_type_code),
@@ -68,7 +68,7 @@ class TestConverter(unittest.TestCase):
 
     def test_split_nodes_delimiter_more_than_one(self):
         node = TextNode("This is text with a `code block` `code block` apples", text_type_text)
-        new_nodes = split_nodes_delimiter([node], "`", text_type_code)
+        new_nodes = converter.split_nodes_delimiter([node], "`", text_type_code)
         expected = [
             TextNode("This is text with a ", text_type_text),
             TextNode("code block", text_type_code),
@@ -78,5 +78,25 @@ class TestConverter(unittest.TestCase):
         ]
         self.assertEqual(new_nodes, expected)
 
+    def test_extract_markdown_images(self):
+        text = "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and ![another](https://i.imgur.com/dfsdkjfd.png)"
+        expected = [('image', 'https://i.imgur.com/zjjcJKZ.png'), ('another', 'https://i.imgur.com/dfsdkjfd.png')]
+        self.assertEqual(expected, converter.extract_markdown_images(text))
+
+    def test_extract_markdown_images_with_links(self):
+        text = "This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)"
+        expected = []
+        self.assertEqual(expected, converter.extract_markdown_images(text))
+
+    def test_extract_markdown_links(self):
+        text = "This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)"
+        expected = [("link", "https://www.example.com"), ("another", "https://www.example.com/another")]
+        self.assertEqual(expected, converter.extract_markdown_links(text))
+
+    def test_extract_markdown_links_with_images(self):
+        text = "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and ![another](https://i.imgur.com/dfsdkjfd.png)"
+        expected = []
+        self.assertEqual(expected, converter.extract_markdown_links(text))
+        
 if __name__ == "__main__":
     unittest.main()
