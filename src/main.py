b0b12a802d5ba858eeb1fa88d025aabac8e688b2
MatheuBotha <matheu.botha@gmail.com>
2024-05-28 13:30:48 +0200

    ADD: generate pages recursively

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import shutil
 import converter
+import os
 
 def copy_static_to_public():
     source_dir = "static"
@@ -13,6 +14,7 @@ def copy_static_to_public():
 
 def write_to_file(path, content):
     try:
+        os.makedirs(os.path.dirname(path), exist_ok=True)
         with open(path, 'w') as f:
             f.write(content)
         print(f"File created successfully: {path}")
@@ -36,10 +38,18 @@ def generate_page(from_path, template_path, dest_path):
             write_to_file(dest_path, template.replace("{{ Content }}", top_html_node.to_html()))
 
 def generate_pages_recursively(from_path, template_path, dest_path):
+    for root, dirs, files in os.walk(from_path):
+        for file in files:
+            if file.endswith('.md'):
+                file_path = os.path.join(root, file)
+                dest_file_path = file_path.replace(from_path, dest_path)
+                dest_file_path = dest_file_path.replace('.md', '.html')
+                generate_page(file_path, template_path, dest_file_path)
     pass
 
 def main():
     copy_static_to_public()
-    generate_page("content/index.md", "public/template.html", "public/index.html")
+    # generate_page("content/index.md", "public/template.html", "public/index.html")
+    generate_pages_recursively("content/", "public/template.html", "public/")
 
 main()
